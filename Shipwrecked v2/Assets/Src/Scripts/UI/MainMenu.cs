e34e9f517a64e97541db934716f8f859c221e085
Thomas Broughton <36638173+Exaiystic@users.noreply.github.com>
2023-09-19 22:59:15 +0100

    Added main menu functionality
    Added functionality for the main menu.

---
+++ Shipwrecked v2/Assets/Src/Scripts/UI/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Settings - General")]
+    [SerializeField] private GameObject _baseMenu;
+    
+    [Header("Settings - PlayButton")]
+    [SerializeField] private string _sceneName;
+    [SerializeField] private GameObject _loadingMenu;
+
+    [Header("Settings - HowToPlayButton")]
+    [SerializeField] private GameObject _howToPlayMenu;
+    
+    public void OnPlay()
+    {
+        SwitchMenu(_baseMenu, _loadingMenu);
+        
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void OnHowToPlayPressed()
+    {
+        SwitchMenu(_baseMenu, _howToPlayMenu);
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnBack()
+    {
+        SwitchMenu(_howToPlayMenu, _baseMenu);
+    }
+
+    private void SwitchMenu(GameObject activeMenu, GameObject targetMenu)
+    {
+        if (activeMenu != null)
+        {
+            activeMenu.SetActive(false);
+
+            if (targetMenu != null)
+            {
+                targetMenu.SetActive(true);
+            }
+        }
+    }
+}
